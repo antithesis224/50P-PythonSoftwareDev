137193ef33200bbbe5a2dda1b640ab8369462826
Colby Koppelmann <colby224@comcast.net>
2023-04-10 23:26:13 -0400

    first try

---
+++ M02Lab.py
@@ -0,0 +1,14 @@
+while(1):
+    studentslastname = input("Input Students Last Name: ")
+    if studentslastname == "ZZZ":
+        break
+    else:
+        studentsfirstname = input("Input Students First Name: ")
+        studentsgpa = input("Input Students GPA: ")
+
+        if studentsgpa >= 3.5:
+            print("Student has made the Dean's List.")
+        elif studentsgpa >= 3.25:
+            print("Student has made the Honor Roll.")
+        else:
+            print("Student did not make Dean's List or Honor Roll.")
